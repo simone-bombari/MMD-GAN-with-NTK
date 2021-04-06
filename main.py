a1bcc8e393064bca66a8964d82b25bb72415858f
Simone Bombari <sbombari@bea81.ista.local>
2021-04-06 22:47:26 +0200

    let's try this

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ weight_decay = 10 ** (-4)
 dataset = 'MNIST'
 batch_size = 512
 noise_batch_size = 512
-sigma = 50
+sigma = 6.5
 latent_size = 50
 train_loader, train_loader_with_replacement, test_loader, labels, num_classes = load_data(dataset, batch_size, download=False)
 epochs = 1
@@ -25,7 +25,8 @@ save_path = './training4.pth'
 
 for epoch in range(epochs):
     print('epoch', epoch, flush=True)
-    noise = torch.randn((noise_batch_size, latent_size)).float().to(device)
+    noise = torch.randn((noise_batch_size, latent_size)).to(device)
+    print(noise.shape)
     print(device, flush=True)
     print(next(net.parameters()).device, flush=True)
     print(noise.device, '\n', flush=True)
@@ -39,8 +40,10 @@ for epoch in range(epochs):
         if c == 110:
             break
         print('minibatch', c, flush=True)  # MNIST has 60000 images
+
         generated_images = net(noise)
-        input_images = input_images.float().to(device)
+        exit(0)
+        input_images = input_images.to(device)
         # generated_images.to(device)
         optimizer.zero_grad()   # zero the gradient buffers
         loss = mmd(input_images.squeeze(), generated_images.squeeze(), sigma)  # Removing the dummy channel dim in MNIST
